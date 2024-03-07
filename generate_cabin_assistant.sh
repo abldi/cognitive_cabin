a86ebc5be40945b146e9e0ad53cf0cc777671315
Simon Moisy <simon.moisy@tutanota.com>
2024-03-07 21:50:21 +0800

    updating model update shell script

--- generate_cabin_assistant.sh
+++ generate_cabin_assistant.sh
@@ -1,3 +1,6 @@
+ollama list
+ollama pull llava
+ollama pull mistral
 ollama rm mistral-cabin-assistant direction-assistant-extractor llava_picture_descriptor.modelfile
 ollama create mistral-cabin-assistant -f mistral_cabin_assistant.modelfile
 ollama create direction-assistant-extractor -f mistral_direction_assistant_extractor.modelfile
