710b6483251e697709d18ca970fd0ab311856aac
Simon Moisy <simon.moisy@tutanota.com>
2024-03-24 16:04:56 +0800

    quick reformat of audio file

--- audio_input_v2.py
+++ audio_input_v2.py
@@ -5,27 +5,28 @@ import webrtcvad as vtad
 import numpy as np
 import io
 
-#Global Constants
-SAMPLE_RATE=16000
+# Global Constants
+SAMPLE_RATE = 16000
 FRAME_DURATION_MS = 20
-CHANNELS=1
+CHANNELS = 1
 CHUNK_SIZE = int(SAMPLE_RATE * FRAME_DURATION_MS / 1000)
 
-#Functions
+
+# Functions
 def obtain_audio():
-    p = pyaudio.PyAudio() #Initialize pyaudio object
+    p = pyaudio.PyAudio()  # Initialize pyaudio object
     stream = p.open(
         format=pyaudio.paInt16,
         channels=CHANNELS,
         rate=SAMPLE_RATE,
         input=True,
         frames_per_buffer=CHUNK_SIZE
-    )#Create a stream to capture data from microphone
+    )  # Create a stream to capture data from microphone
     try:
         while True:
             # Read audio from stream
             audio_data = stream.read(CHUNK_SIZE)
-            #Creates a generator(iterator), function continues when data is iterated
+            # Creates a generator(iterator), function continues when data is iterated
             yield audio_data
     finally:
         # Closes when termination
@@ -33,32 +34,35 @@ def obtain_audio():
         stream.close()
         p.terminate()
 
-def check_speech(iterator,vad):
+
+def check_speech(iterator, vad):
     try:
         audio_data = next(iterator)
         audio_data_bytes = np.frombuffer(audio_data, dtype=np.int16).tobytes()
-        return(vad.is_speech(audio_data_bytes,SAMPLE_RATE))  
+        return (vad.is_speech(audio_data_bytes, SAMPLE_RATE))
     except StopIteration:
-        return False  
+        return False
 
-def transcribe(model, iterator,vad):
-    #Obtaining whole phrase
-    acum = [] #variable to store the audio for the phrase
+
+def transcribe(model, iterator, vad):
+    # Obtaining whole phrase
+    acum = []  # variable to store the audio for the phrase
     while True:
         try:
             audio_data = next(iterator)
             audio_data_bytes = np.frombuffer(audio_data, dtype=np.int16).tobytes()
-            if not (vad.is_speech(audio_data_bytes,SAMPLE_RATE)):#silence detected - MIGHT NEED FUTURE CHANGES TO RECOGNIZE OR WAIT TO SAY IT IS SILENCE
+            if not (vad.is_speech(audio_data_bytes,
+                                  SAMPLE_RATE)):  # silence detected - MIGHT NEED FUTURE CHANGES TO RECOGNIZE OR WAIT TO SAY IT IS SILENCE
                 break
             acum.append(audio_data_bytes)
         except StopIteration:
             break
-    #Formatting
+    # Formatting
     audio = b''.join(acum)
     audio_np = np.frombuffer(audio, dtype=np.int16).astype(np.float32)
     audio_np /= np.iinfo(np.int16).max
-    #Transcription
-    segments, info = model.transcribe(audio_np,length_penalty= 0.1,beam_size=5,language="en")
+    # Transcription
+    segments, info = model.transcribe(audio_np, length_penalty=0.1, beam_size=5, language="en")
     try:
         first_segment = next(segments)
         text = first_segment.text
@@ -66,17 +70,18 @@ def transcribe(model, iterator,vad):
     except StopIteration:
         return ""
 
-#Main execution
+
+# Main execution
 model_size = "medium.en"
-model = fw.WhisperModel(model_size,device="cuda",compute_type="float16")
+model = fw.WhisperModel(model_size, device="cuda", compute_type="float16")
 vad = vtad.Vad()
-vad.set_mode(3)#Mode 3 to be more aggresive when recognizing what is voice or noise
+vad.set_mode(3)  # Mode 3 to be more aggresive when recognizing what is voice or noise
 iterator = obtain_audio()
 acum_translation = ""
 while True:
     try:
-        if(check_speech(iterator,vad)):
-            translation=transcribe(model,iterator,vad)
+        if (check_speech(iterator, vad)):
+            translation = transcribe(model, iterator, vad)
             acum_translation += translation
             print(translation)
     except KeyboardInterrupt:
